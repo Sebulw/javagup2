8e809208dad8dfa9080539ace39ffb0886acc26a
xenteros <adam@gurgul.com.pl>
2018-08-21 10:04:02 +0200

    Add FileReaders (#3)
    * Created common interface,
    * Created abstract reader,
    * Created two implementations

---
+++ src/main/java/password/manager/files/AbstractFileReader.java
@@ -0,0 +1,14 @@
+package password.manager.files;
+
+import java.io.File;
+
+abstract class AbstractFileReader implements FileReader {
+
+    protected File getFile(String path) {
+        if (path == null) {
+            throw new IllegalArgumentException("May not be null");
+        }
+        return new File(path);
+    }
+
+}
